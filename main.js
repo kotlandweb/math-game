85b4d4cc7cc943438623e59165b05754407f92fc
.... <150514422+Oskar667@users.noreply.github.com>
2025-12-05 21:37:56 +0200

    Main update

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 document.getElementById("btn").addEventListener("click", function () {
     document.getElementById("text").textContent = "JavaScript veikia!";
 });
+window.addEventListener('load', () => {
+  const snowContainer = document.querySelector('.snow');
+  
+  for (let i = 0; i < 100; i++) {
+    let snowflake = document.createElement('div');
+    snowContainer.appendChild(snowflake);
+  }
+});
